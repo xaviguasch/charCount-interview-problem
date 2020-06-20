c07a1bdd7a877071da02b2d4165d525cf9f3608f
xaviguasch <xaviguasch@gmail.com>
2020-06-20 19:59:47 +0200

    added solution for problem

--- charCount.js
+++ charCount.js
@@ -1,5 +1,19 @@
-function charCount(str) {}
+// Write a function which takes in a string and returns counts of each character in the string.
+
+function charCount(str) {
+  const resultObj = {}
+
+  let arrChars = str.toLowerCase().split('')
+
+  arrChars.forEach((char) => {
+    if (Object.keys(resultObj).includes(char)) {
+      resultObj[char]++
+    } else {
+      resultObj[char] = 1
+    }
+  })
+
+  return resultObj
+}
 
 console.log(charCount('Romino Rovirosa'))
-
-// Write a function which takes in a string and returns counts of each character in the string.
